914a879ba4f4ad54f87117118a5936c124ff0bcd
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-16 18:35:27 +0100

    Fixes, added modlist and fixed some typos.

--- index.php
+++ index.php
@@ -21,129 +21,237 @@
 	</head>
 	<body>
 		<!-- Header -->
-			<section id="header" class="dark">
-				<header>
-					<h1>Velkommen til VolzPlay!</h1>
-					<p>En moddet Minecraft server for deg som liker utfordringer og teknologi rettete modder.</p>
-				</header>
-				<footer>
-					<a href="#first" class="button scrolly">Finn ut mer</a>
-				</footer>
-			</section>
+		<section id="header" class="dark">
+			<header>
+				<h1>Velkommen til VoltzPlay!</h1>
+				<p>En moddet Minecraft server for deg som liker utfordringer og teknologi rettete modder.</p>
+			</header>
+			<footer>
+				<a href="#information" class="button scrolly">Finn ut mer</a>
+			</footer>
+		</section>
 			
-		<!-- First -->
-			<section id="first" class="main">
-				<header>
-					<div class="container">
-						<h2>Hvordan blir jeg med på å spille?</h2>
-						<p>For å kunne spille på vår server må du laste ned vår <a href="#launcher">launcher</a>, og du må spørre "halvorshalvors" for at du skal komme på vår whitelist.</p>
-					</div>
-				</header>
-				<div class="content dark style1 featured">
-					<div class="container">
-						<div class="row">
-							<div id="launcher" class="4u">
-								<section>
-									<span class="feature-icon"><span class="icon fa-cloud"></span></span>
-									<header>
-										<h3>Launcher</h3>
-									</header>
-									<p>Vi har vår egen Launcher, det vil si at vi sørger for å starte spillet med de nødvendige og oppdaterte moddene for deg.</p>
-									<a href="launcher/VoltzPlay! Launcher.exe" class="button">Windows</a></p>
-									<a href="launcher/VoltzPlay! Launcher.jar" class="button">Andre OS</a></p>
-								</section>
-							</div>
-							<div class="4u">
-								<section>
-									<span class="feature-icon"><span class="icon fa-clock-o"></span></span>
-									<header>
-										<h3>Regler</h3>
-									</header>
-									<p>Du må lese nøye igjennom og godta reglene før du begynner å spille.</p>
-									<a href="#rules" class="button scrolly">Vis regler</a>
-								</section>
-							</div>
-							<div class="4u">
-								<section>
-									<span class="feature-icon"><span class="icon fa-bolt"></span></span>
-									<header>
-										<h3>Kart</h3>
-									</header>
-									<p>Kartet gir deg en enkel oversikt over verden, det lar deg også se posisjonen til andre spillere.</p>
-									<a href="map" class="button">Vis kart</a>
-								</section>
-							</div>
+		<!-- Information -->
+		<section id="information" class="main">
+			<header>
+				<div class="container">
+					<h2>Hvordan blir jeg med på å spille?</h2>
+					<p>For å kunne spille på vår server må du laste ned vår <a href="#launcher">launcher</a>, og spørre "halvorshalvors" om å komme på vår whitelist.</p>
+				</div>
+			</header>
+			<div class="content dark style1 featured">
+				<div class="container">
+					<div class="row">
+						<div id="launcher" class="4u">
+							<section>
+								<span class="feature-icon"><span class="icon fa-cloud"></span></span>
+								<header>
+									<h3>Launcher</h3>
+								</header>
+								<p>Vi har vår egen Launcher, det vil si at vi sørger for å starte spillet med de nødvendige og oppdaterte moddene for deg.</p>
+								<a href="launcher/VoltzPlay! Launcher.exe" class="button">Windows</a></p>
+								<a href="launcher/VoltzPlay! Launcher.jar" class="button">Andre OS</a></p>
+							</section>
+						</div>
+						<div class="4u">
+							<section>
+								<span class="feature-icon"><span class="icon fa-clock-o"></span></span>
+								<header>
+									<h3>Regler</h3>
+								</header>
+								<p>Du må lese nøye igjennom og godta reglene før du begynner å spille.</p>
+								<a href="#rules" class="button scrolly">Vis regler</a>
+							</section>
+						</div>
+						<div class="4u">
+							<section>
+								<span class="feature-icon"><span class="icon fa-bolt"></span></span>
+								<header>
+									<h3>Kart</h3>
+								</header>
+								<p>Kartet gir deg en enkel oversikt over verden, det lar deg også se posisjonen til andre spillere.</p>
+								<a href="map" class="button">Vis kart</a>
+							</section>
 						</div>
 					</div>
 				</div>
-			</section>
-			<section id="rules" class="main">
-				<header>
-					<div class="container">
-						<h2>Regler</h2>
-						<p>Les nøye igjennom og godta reglene. Nye regler kan komme underveis.</p>
-					</div>
-				</header>
-				<div class="content style1 dark">
-					<div class="container">
-						<section>
-							<header>
-								<h3>Grunnleggende</h3>
-							</header>
-							<ul class="default">
-								<li>Tyveri er av andre spilleres eiendom eller gjenstander er ikke lov.</li>
-								<li>Ikke grief andre spilleres eiendom eller byggverk.</li>
-								<li>Ikke drep/sloss mot andre, med mindre dette er avtalt.</li>
-								<li>Ikke forårsak lagg på serveren, prøv å holde antallet maskiner og ressurskrevende blokker nede.</li>
-								<li>Ikke gå i eller endre på andres hus eller eiendom, med mindre dette er avtalt.</li>
-								<li>Snakk ordentlig, hold språket rent.</li>
-								<li>Du er ansvarlig for alt som er forårsaket av din Minecraft bruker, selv om en annen person låner den.</li>
-								<li>Følg alltid norsk lov.</li>
-							</ul>
-						</section>
-						<section>
-							<header>
-								<h3>Utvidede</h3>
-							</header>
-							<ul class="default">
-								<li>Ikke rør/grief vulkaner på noen måte, disse er fredet pga de kommer ikke tilbake om de blir ødelagt.</li>
-							</ul>
-						</section>
-					</div>
+			</div>
+		</section>
+		
+		<!-- Rules -->
+		<section id="rules" class="main">
+			<header>
+				<div class="container">
+					<h2>Regler</h2>
+					<p>Les nøye igjennom og godta reglene. Nye regler kan komme underveis.</p>
 				</div>
-			</section>
-			<section id="rules" class="main">
-				<header>
-					<div class="container">
-						<h2>Modder</h2>
-						<p>En liste over moddene vi bruker på serveren, kun for informasjon/referanse da moddene blir installert automatisk av vår <a href="#launcher">launcher</a>.</p>
-					</div>
-				</header>
-				<div class="content style1 dark">
-					<div class="container">
-						<section>
-							<div class="table-wrapper">
-								<table class="default">
-									<thead>
-										<tr>
-											<th>Navn</th>
-											<th>Versjon</th>
-											<th>Dokumentasjon</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>Applied Energistics 2</td>
-											<td>rv2-beta-13</td>
-											<td><a href="http://ae-mod.info/">http://ae-mod.info/</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</section>
-					</div>
+			</header>
+			<div class="content style1 dark">
+				<div class="container">
+					<section>
+						<header>
+							<h3>Grunnleggende</h3>
+						</header>
+						<ul class="default">
+							<li>Tyveri er av andre spilleres eiendom eller gjenstander er ikke lov.</li>
+							<li>Ikke grief andre spilleres eiendom eller byggverk.</li>
+							<li>Ikke drep/sloss mot andre, med mindre dette er avtalt.</li>
+							<li>Ikke forårsak lagg på serveren, prøv å holde antallet maskiner og ressurskrevende blokker nede.</li>
+							<li>Ikke gå i eller endre på andres hus eller eiendom, med mindre dette er avtalt.</li>
+							<li>Snakk ordentlig, hold språket rent.</li>
+							<li>Du er ansvarlig for alt som er forårsaket av din Minecraft bruker, selv om en annen person låner den.</li>
+							<li>Følg alltid norsk lov.</li>
+						</ul>
+					</section>
+					<section>
+						<header>
+							<h3>Utvidede</h3>
+						</header>
+						<ul class="default">
+							<li>Ikke rør/grief vulkaner på noen måte, disse er fredet pga de kommer ikke tilbake om de blir ødelagt.</li>
+						</ul>
+					</section>
 				</div>
-			</section>
+			</div>
+		</section>
+		
+		<!-- Mods -->
+		<section class="main">
+			<header>
+				<div class="container">
+					<h2>Modder</h2>
+					<p>En liste over moddene vi bruker på serveren, kun for informasjon/referanse da moddene blir installert automatisk av vår <a href="#launcher">launcher</a>.</p>
+				</div>
+			</header>
+			<div class="content style1 dark">
+				<div class="container">
+					<section>
+						<table class="default">
+							<thead>
+								<tr>
+									<th>Navn</th>
+									<th>Versjon</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><a href="http://ae-mod.info/">AppliedEnergistics 2</a></td>
+									<td>rv2-beta-13</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/69162-cofhcore">CoFHCore</a></td>
+									<td>3.0.0RC7-211</td>
+								</tr>
+								<tr>
+									<td><a href="http://calclavia.com/electrodynamics/">Electrodynamics</a></td>
+									<td>N/A</td>
+								</tr>
+								<tr>
+									<td><a href="http://micdoodle8.com/mods/galacticraft">Galacticraft-Planets</a></td>
+									<td>3.0.8.265</td>
+								</tr>
+								<tr>
+									<td><a href="http://micdoodle8.com/mods/galacticraft">Galacticraft-Core</a></td>
+									<td>3.0.8.265</td>
+								</tr>
+								<tr>
+									<td><a href="http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/1280827-1-5-and-up-forge-universal-ironchests-5-0">IronChests</a></td>
+									<td>6.0.62.742</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/74924-mantle">Mantle</a></td>
+									<td>0.3.2</td>
+								</tr>
+								<tr>
+									<td><a href="http://aidancbrady.com/mekanism/">Mekanism</a></td>
+									<td>7.1.2.185</td>
+								</tr>
+								<tr>    
+									<td><a href="http://aidancbrady.com/mekanism/">Mekanism Generators</a></td>
+									<td>7.1.2.185</td>
+								</tr>
+								<tr>    
+									<td><a href="http://aidancbrady.com/mekanism/">Mekanism Tools</a></td>
+									<td>7.1.2.185</td>
+								</tr>
+								<tr>
+									<td><a href="http://calclavia.com/mffs/">Modular Force Field System</a></td>
+									<td>N/A</td>
+								</tr>
+								<tr>
+									<td><a href="http://micdoodle8.com/mods/galacticraft">MicdoodleCore</a></td>
+									<td>3.0.8.265</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/73373-minechem">MineChem</a></td>
+									<td>5.0.5.385</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/66675-netherores">NetherOres</a></td>
+									<td>2.3.0RC4-4</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/223008-opencomputers">OpenComputers</a></td>
+									<td>1.5.3.10</td>
+								</tr>
+								<tr>
+									<td><a href="http://projectredwiki.com/wiki/Main_Page">ProjectRed-Base</a></td>
+									<td>4.5.14.66</td>
+								</tr>
+								<tr>
+									<td><a href="http://projectredwiki.com/wiki/Main_Page">ProjectRed-Compat</a></td>
+									<td>4.5.14.66</td>
+								</tr>
+								<tr>
+									<td><a href="http://projectredwiki.com/wiki/Main_Page">ProjectRed-Integration</a></td>
+									<td>4.5.14.66</td>
+								</tr>
+								<tr>
+									<td><a href="http://projectredwiki.com/wiki/Main_Page">ProjectRed-Lighting</a></td>
+									<td>4.5.14.66</td>
+								</tr>
+								<tr>
+									<td><a href="http://projectredwiki.com/wiki/Main_Page">ProjectRed-Mechanical_beta</a></td>
+									<td>4.5.14.66</td>
+								</tr>
+								<tr>
+									<td><a href="http://projectredwiki.com/wiki/Main_Page">ProjectRed-World</a></td>
+									<td>4.5.14.66</td>
+								</tr>
+								<tr>
+									<td><a href="http://www.railcraft.info/">Railcraft</a></td>
+									<td>9.5.0.0</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/70631-redstone-arsenal">Redstone Arsenal</a></td>
+									<td>1.1.0RC7-65</td>
+								</tr>
+								<tr>
+									<td><a href="http://resonantengine.com/">Resonant Engine</a></td>
+									<td>N/A</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/74072-tinkers-construct">Tinkers Construct</a></td>
+									<td>1.8.3b</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/227443-thermal-dynamics">Thermal Dynamics</a></td>
+									<td>1.0.0RC7-98</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/69163-thermalexpansion">Thermal Expansion</a></td>
+									<td>4.0.0RC7-141</td>
+								</tr>
+								<tr>
+									<td><a href="http://minecraft.curseforge.com/mc-mods/222880-thermal-foundation">Thermal Foundation</a></td>
+									<td>1.0.0RC7-62</td>
+								</tr>
+							</tbody>
+						</table>
+					</section>
+				</div>
+			</div>
+		</section>
 
 		<!-- Basic Elements -->
 		<!--
@@ -318,18 +426,15 @@
 		-->
 			
 		<!-- Footer -->
-			<section id="footer">
-				<ul class="icons">
-					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="https://github.com/VoltzPlay/" class="icon fa-github"><span class="label">GitHub</span></a></li>
+		<section id="footer">
+			<ul class="icons">
+				<li><a href="https://github.com/VoltzPlay/" class="icon fa-github"><span class="label">GitHub</span></a></li>
+			</ul>
+			<div class="copyright">
+				<ul class="menu">
+					<li>Kopirett &copy; 2014-<?php echo date('Y'); ?> Halvor Lyche Strandvoll.</li>
 				</ul>
-				<div class="copyright">
-					<ul class="menu">
-						<li>Kopirett &copy; <?php echo date('Y'); ?> VoltzPlay!</li>
-					</ul>
-				</div>
-			</section>
-
+			</div>
+		</section>
 	</body>
 </html>
