201290c4b84f16fec4bb68ce02d27ee6082c0b93
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-16 15:15:54 +0100

    Initial commit of new site.

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>VoltzPlay!</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<link href="favicon.ico" rel="icon" type="image/x-icon" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+		<!-- Header -->
+			<section id="header" class="dark">
+				<header>
+					<h1>Velkommen til VolzPlay!</h1>
+					<p>En moddet Minecraft server for deg som liker utfordringer og teknologi rettete modder.</p>
+				</header>
+				<footer>
+					<a href="#first" class="button scrolly">Finn ut mer</a>
+				</footer>
+			</section>
+			
+		<!-- First -->
+			<section id="first" class="main">
+				<header>
+					<div class="container">
+						<h2>Hvordan blir jeg med på å spille?</h2>
+						<p>For å kunne spille på vår server må du laste ned vår <a href="#launcher">launcher</a>, og du må spørre "halvorshalvors" for at du skal komme på vår whitelist.</p>
+					</div>
+				</header>
+				<div class="content dark style1 featured">
+					<div class="container">
+						<div class="row">
+							<div id="launcher" class="4u">
+								<section>
+									<span class="feature-icon"><span class="icon fa-cloud"></span></span>
+									<header>
+										<h3>Launcher</h3>
+									</header>
+									<p>Vi har vår egen Launcher, det vil si at vi sørger for å starte spillet med de nødvendige og oppdaterte moddene for deg.</p>
+									<a href="launcher/VoltzPlay! Launcher.exe" class="button">Windows</a></p>
+									<a href="launcher/VoltzPlay! Launcher.jar" class="button">Andre OS</a></p>
+								</section>
+							</div>
+							<div class="4u">
+								<section>
+									<span class="feature-icon"><span class="icon fa-clock-o"></span></span>
+									<header>
+										<h3>Regler</h3>
+									</header>
+									<p>Du må lese nøye igjennom og godta reglene før du begynner å spille.</p>
+									<a href="#rules" class="button">Vis regler</a>
+								</section>
+							</div>
+							<div class="4u">
+								<section>
+									<span class="feature-icon"><span class="icon fa-bolt"></span></span>
+									<header>
+										<h3>Kart</h3>
+									</header>
+									<p>Kartet gir deg en enkel oversikt over verden, det lar deg også se posisjonen til andre spillere.</p>
+									<a href="map" class="button">Vis kart</a>
+								</section>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="rules" class="main">
+				<header>
+					<div class="container">
+						<h2>Regler</h2>
+						<p>Les nøye igjennom og godta reglene. Nye regler kan komme underveis.</p>
+					</div>
+				</header>
+				<div class="content style1 dark">
+					<div class="container">
+						<section>
+							<header>
+								<h3>Grunnleggende</h3>
+							</header>
+							<ul class="default">
+								<li>Tyveri er av andre spilleres eiendom eller gjenstander er ikke lov.</li>
+								<li>Ikke grief andre spilleres eiendom eller byggverk.</li>
+								<li>Ikke drep/sloss mot andre, med mindre dette er avtalt.</li>
+								<li>Ikke forårsak lagg på serveren, prøv å holde antallet maskiner og ressurskrevende blokker nede.</li>
+								<li>Ikke gå i eller endre på andres hus eller eiendom, med mindre dette er avtalt.</li>
+								<li>Snakk ordentlig, hold språket rent.</li>
+								<li>Du er ansvarlig for alt som er forårsaket av din Minecraft bruker, selv om en annen person låner den.</li>
+								<li>Følg norsk lov.</li>
+							</ul>
+						</section>
+						<section>
+							<header>
+								<h3>Spesifikke</h3>
+							</header>
+							<ul class="default">
+								<li>Tyveri er av andre spilleres eiendom eller gjenstander er ikke lov.</li>
+								<li>Ikke grief andre spilleres eiendom eller byggverk.</li>
+								<li>Ikke drep/sloss mot andre, med mindre dette er avtalt.</li>
+								<li>Ikke forårsak lagg på serveren, prøv å holde antallet maskiner og ressurskrevende blokker nede.</li>
+								<li>Ikke gå i eller endre på andres hus eller eiendom, med mindre dette er avtalt.</li>
+								<li>Snakk ordentlig, hold språket rent.</li>
+								<li>Du er ansvarlig for alt som er forårsaket av din Minecraft bruker, selv om en annen person låner den.</li>
+								<li>Følg norsk lov.</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- Basic Elements -->
+		<!--
+			<section class="main">
+				<header>
+					<div class="container">
+						<h2>A Whole Lotta Elements</h2>
+						<p>General purpose elements for every general purpose. Or something like that.</p>
+					</div>
+				</header>
+				<div class="content style1 dark">
+					<div class="container">
+						<section>
+							<header>
+								<h3>Paragraph</h3>
+								<p>This is a byline</p>
+							</header>
+							<p>Phasellus nisl nisl, varius id <sup>porttitor sed pellentesque</sup> ac orci. Pellentesque 
+							habitant <strong>strong</strong> tristique <b>bold</b> et netus <i>italic</i> malesuada <em>emphasized</em> ac turpis egestas. Morbi 
+							leo suscipit ut. Praesent <sub>id turpis vitae</sub> turpis pretium ultricies. Vestibulum sit 
+							amet risus elit.</p>
+						</section>
+						<section>
+							<header>
+								<h3>Blockquote</h3>
+							</header>
+							<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget.
+							tempus euismod. Vestibulum ante ipsum primis in faucibus. Cras sit amet urna eros, id egestas
+							tempus ante ipsum primis in faucibus orci luctus et ultrices.</blockquote>
+						</section>
+						<section>
+							<header>
+								<h3>Divider</h3>
+							</header>
+							<p>Donec consectetur <a href="#">vestibulum dolor et pulvinar</a>. Etiam vel felis enim, at viverra 
+							ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci 
+							facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam 
+							tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
+							posuere cubilia.</p>
+							<hr />
+							<p>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra 
+							ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci 
+							facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam 
+							tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
+							posuere cubilia.</p>
+						</section>
+						<section>
+							<header>
+								<h3>Unordered List</h3>
+							</header>
+							<ul class="default">
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+							</ul>
+						</section>
+						<section>
+							<header>
+								<h3>Ordered List</h3>
+							</header>
+							<ol class="default">
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
+							</ol>
+						</section>
+						<section>
+							<header>
+								<h3>Table</h3>
+							</header>
+							<div class="table-wrapper">
+								<table class="default">
+									<thead>
+										<tr>
+											<th>ID</th>
+											<th>Name</th>
+											<th>Description</th>
+											<th>Price</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>00001</td>
+											<td>Lorem ipsum dolor</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>29.99</td>
+										</tr>
+										<tr>
+											<td>00002</td>
+											<td>Sit amet nullam</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>19.99</td>
+										</tr>
+										<tr>
+											<td>00003</td>
+											<td>Feugiat felis viverra</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>29.99</td>
+										</tr>
+										<tr>
+											<td>00004</td>
+											<td>Sagittis enim felis</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>19.99</td>
+										</tr>
+										<tr>
+											<td>00005</td>
+											<td>Nullam sed vestibulum</td>
+											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
+											<td>19.99</td>
+										</tr>
+									</tbody>
+									<tfoot>
+										<tr>
+											<td colspan="3"></td>
+											<td>100.00</td>
+										</tr>
+									</tfoot>
+								</table>
+							</div>
+						</section>
+						<section>
+							<header>
+								<h3>Form</h3>
+							</header>
+							<form method="post" action="#">
+								<div class="row 50%">
+									<div class="6u">
+										<input type="text" name="name" id="name" value="" placeholder="John Doe" />
+									</div>
+									<div class="6u">
+										<input type="text" name="email" id="email" value="" placeholder="johndoe@domain.tld" />
+									</div>
+								</div>
+								<div class="row 50%">
+									<div class="12u">
+										<div class="select" tabindex="-1">
+											<select name="department" id="department">
+												<option value="">Choose a department</option>
+												<option value="1">Manufacturing</option>
+												<option value="2">Administration</option>
+												<option value="3">Support</option>
+											</select>
+										</div>
+									</div>
+								</div>
+								<div class="row 50%">
+									<div class="12u">
+										<input type="text" name="subject" id="subject" value="" placeholder="Enter your subject" />
+									</div>
+								</div>
+								<div class="row 50%">
+									<div class="12u">
+										<textarea name="message" id="message" placeholder="Enter your message"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<ul class="actions">
+											<li><input type="submit" class="button" value="Send Message" /></li>
+											<li><input type="reset" class="button alt" value="Clear Form" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</section>
+					</div>
+				</div>
+			</section>
+		-->
+			
+		<!-- Footer -->
+			<section id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+				</ul>
+				<div class="copyright">
+					<ul class="menu">
+						<li>Kopirett &copy; <?php echo date('Y'); ?> VoltzPlay!</li>
+					</ul>
+				</div>
+			</section>
+
+	</body>
+</html>
