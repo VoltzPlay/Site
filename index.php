5cb9d66839059ab4e404611ec115790fa6ff7743
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-17 13:43:37 +0100

    Lot of improvements, added more pictures.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	<head>
 		<title>VoltzPlay!</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<meta name="description" content="">
-		<meta name="keywords" content="">
+		<meta name="description" content="En moddet Minecraft server for deg som liker utfordringer og teknologi rettede modder.">
+		<meta name="keywords" content="minecraft, voltzplay, forge, mods, server">
 		<link rel="shortcut icon" href="images/favicon.ico">
 		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
 		<script src="js/jquery.min.js"></script>
@@ -24,7 +24,7 @@
 		<section id="header" class="dark">
 			<header>
 				<h1>Velkommen til VoltzPlay!</h1>
-				<p>En moddet Minecraft server for deg som liker utfordringer og teknologi rettete modder.</p>
+				<p>En moddet Minecraft server for deg som liker utfordringer og teknologi rettede modder.</p>
 			</header>
 			<footer>
 				<a href="#information" class="button scrolly">Finn ut mer</a>
@@ -44,7 +44,7 @@
 					<div class="row">
 						<div id="launcher" class="4u">
 							<section>
-								<span class="feature-icon"><span class="icon fa-cloud"></span></span>
+								<span class="feature-icon"><a href="launcher/VoltzPlay! Launcher.jar" class="icon fa-cloud-download"></a></span>
 								<header>
 									<h3>Launcher</h3>
 								</header>
@@ -55,7 +55,7 @@
 						</div>
 						<div class="4u">
 							<section>
-								<span class="feature-icon"><span class="icon fa-clock-o"></span></span>
+								<span class="feature-icon"><a href="#rules" class="icon fa-book"></a></span>
 								<header>
 									<h3>Regler</h3>
 								</header>
@@ -65,7 +65,7 @@
 						</div>
 						<div class="4u">
 							<section>
-								<span class="feature-icon"><span class="icon fa-bolt"></span></span>
+								<span class="feature-icon"><a href="map" class="icon fa-map-marker"></a></span>
 								<header>
 									<h3>Kart</h3>
 								</header>
@@ -83,10 +83,10 @@
 			<header>
 				<div class="container">
 					<h2>Regler</h2>
-					<p>Les nøye igjennom og godta reglene. Nye regler kan komme underveis.</p>
+					<p>Les nøye igjennom og godta reglene. Vi tar for behold om at nye regler kan komme uten varsel.</p>
 				</div>
 			</header>
-			<div class="content style1 dark">
+			<div class="content style2 dark">
 				<div class="container">
 					<section>
 						<header>
@@ -123,7 +123,7 @@
 					<p>En liste over moddene vi bruker på serveren, kun for informasjon/referanse da moddene blir installert automatisk av vår <a href="#launcher">launcher</a>.</p>
 				</div>
 			</header>
-			<div class="content style1 dark">
+			<div class="content dark">
 				<div class="container">
 					<section>
 						<table class="default">
@@ -252,183 +252,12 @@
 				</div>
 			</div>
 		</section>
-
-		<!-- Basic Elements -->
-		<!--
-			<section class="main">
-				<header>
-					<div class="container">
-						<h2>A Whole Lotta Elements</h2>
-						<p>General purpose elements for every general purpose. Or something like that.</p>
-					</div>
-				</header>
-				<div class="content style1 dark">
-					<div class="container">
-						<section>
-							<header>
-								<h3>Paragraph</h3>
-								<p>This is a byline</p>
-							</header>
-							<p>Phasellus nisl nisl, varius id <sup>porttitor sed pellentesque</sup> ac orci. Pellentesque 
-							habitant <strong>strong</strong> tristique <b>bold</b> et netus <i>italic</i> malesuada <em>emphasized</em> ac turpis egestas. Morbi 
-							leo suscipit ut. Praesent <sub>id turpis vitae</sub> turpis pretium ultricies. Vestibulum sit 
-							amet risus elit.</p>
-						</section>
-						<section>
-							<header>
-								<h3>Blockquote</h3>
-							</header>
-							<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget.
-							tempus euismod. Vestibulum ante ipsum primis in faucibus. Cras sit amet urna eros, id egestas
-							tempus ante ipsum primis in faucibus orci luctus et ultrices.</blockquote>
-						</section>
-						<section>
-							<header>
-								<h3>Divider</h3>
-							</header>
-							<p>Donec consectetur <a href="#">vestibulum dolor et pulvinar</a>. Etiam vel felis enim, at viverra 
-							ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci 
-							facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam 
-							tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
-							posuere cubilia.</p>
-							<hr />
-							<p>Donec consectetur vestibulum dolor et pulvinar. Etiam vel felis enim, at viverra 
-							ligula. Ut porttitor sagittis lorem, quis eleifend nisi ornare vel. Praesent nec orci 
-							facilisis leo magna. Cras sit amet urna eros, id egestas urna. Quisque aliquam 
-							tempus euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
-							posuere cubilia.</p>
-						</section>
-						<section>
-							<header>
-								<h3>Unordered List</h3>
-							</header>
-							<ul class="default">
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-							</ul>
-						</section>
-						<section>
-							<header>
-								<h3>Ordered List</h3>
-							</header>
-							<ol class="default">
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-								<li>Donec consectetur vestibulum dolor et vel felis enim at viverra ligula. Ut porttitor sagittis lorem.</li>
-							</ol>
-						</section>
-						<section>
-							<header>
-								<h3>Table</h3>
-							</header>
-							<div class="table-wrapper">
-								<table class="default">
-									<thead>
-										<tr>
-											<th>ID</th>
-											<th>Name</th>
-											<th>Description</th>
-											<th>Price</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>00001</td>
-											<td>Lorem ipsum dolor</td>
-											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
-											<td>29.99</td>
-										</tr>
-										<tr>
-											<td>00002</td>
-											<td>Sit amet nullam</td>
-											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
-											<td>19.99</td>
-										</tr>
-										<tr>
-											<td>00003</td>
-											<td>Feugiat felis viverra</td>
-											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
-											<td>29.99</td>
-										</tr>
-										<tr>
-											<td>00004</td>
-											<td>Sagittis enim felis</td>
-											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
-											<td>19.99</td>
-										</tr>
-										<tr>
-											<td>00005</td>
-											<td>Nullam sed vestibulum</td>
-											<td>Ut porttitor sagittis lorem quis nisi ornare.</td>
-											<td>19.99</td>
-										</tr>
-									</tbody>
-									<tfoot>
-										<tr>
-											<td colspan="3"></td>
-											<td>100.00</td>
-										</tr>
-									</tfoot>
-								</table>
-							</div>
-						</section>
-						<section>
-							<header>
-								<h3>Form</h3>
-							</header>
-							<form method="post" action="#">
-								<div class="row 50%">
-									<div class="6u">
-										<input type="text" name="name" id="name" value="" placeholder="John Doe" />
-									</div>
-									<div class="6u">
-										<input type="text" name="email" id="email" value="" placeholder="johndoe@domain.tld" />
-									</div>
-								</div>
-								<div class="row 50%">
-									<div class="12u">
-										<div class="select" tabindex="-1">
-											<select name="department" id="department">
-												<option value="">Choose a department</option>
-												<option value="1">Manufacturing</option>
-												<option value="2">Administration</option>
-												<option value="3">Support</option>
-											</select>
-										</div>
-									</div>
-								</div>
-								<div class="row 50%">
-									<div class="12u">
-										<input type="text" name="subject" id="subject" value="" placeholder="Enter your subject" />
-									</div>
-								</div>
-								<div class="row 50%">
-									<div class="12u">
-										<textarea name="message" id="message" placeholder="Enter your message"></textarea>
-									</div>
-								</div>
-								<div class="row">
-									<div class="12u">
-										<ul class="actions">
-											<li><input type="submit" class="button" value="Send Message" /></li>
-											<li><input type="reset" class="button alt" value="Clear Form" /></li>
-										</ul>
-									</div>
-								</div>
-							</form>
-						</section>
-					</div>
-				</div>
-			</section>
-		-->
 			
 		<!-- Footer -->
 		<section id="footer">
 			<ul class="icons">
 				<li><a href="https://github.com/VoltzPlay/" class="icon fa-github"><span class="label">GitHub</span></a></li>
+				<li><a href="bitcoin:1MBQoKGYf4PJi9cHhpENL7K2csGHavXbm6" class="icon fa-bitcoin"><span class="label">Bitcoin</span></a></li>
 			</ul>
 			<div class="copyright">
 				<ul class="menu">
